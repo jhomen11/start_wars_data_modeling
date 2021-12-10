7ce2d0fe75b3a5504c12d22eb609436ec6ea6133
Jhonny Mendoza <jhomen11@gmail.com>
2021-12-10 22:20:12 +0000

    Data-modeling star wars listo

--- src/models.py
+++ src/models.py
@@ -8,23 +8,46 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class People(Base):
+    __tablename__ = 'people'
+    uid = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    
+class Planets(Base):
+    __tablename__ = 'planets'
+    uid = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    uid = Column(Integer, primary_key=True)
+    model = Column(String(250), nullable=False)
+    starship_class = Column(String(250), nullable=False)
+    passengers = Column(Integer, nullable=False)
+
+class FavoriteList(Base):
+    __tablename__ = 'favoriteList'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    people_id = Column(Integer, ForeignKey('people.uid'))
+    planets_id = Column(Integer, ForeignKey('planets.uid'))
+    starships_id = Column(Integer, ForeignKey('starships.uid'))
+    people = relationship(People)
+    planets = relationship(Planets)
+    starships = relationship(Starships)
+    user = relationship(User)
 
     def to_dict(self):
         return {}
